0a31437001802e9b926aa8c90e7afa5611003f4c
장윤정 <wkddbs3935@naver.com>
2025-03-26 21:04:06 +0900

    initial commit

---
+++ src/test/java/deepnyangning/capstonebe/CapstoneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package deepnyangning.capstonebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
