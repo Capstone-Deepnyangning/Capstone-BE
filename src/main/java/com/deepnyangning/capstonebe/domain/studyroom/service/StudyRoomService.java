aef1d0e65136f1a7f27f38d97557e4dc18b42fbe
장윤정 <wkddbs3935@naver.com>
2025-04-12 04:51:35 +0900

    feat: 예약 가능한 스터디룸 조회 기능

--- src/main/java/com/deepnyangning/capstonebe/domain/studyroom/service/StudyRoomService.java
+++ src/main/java/com/deepnyangning/capstonebe/domain/studyroom/service/StudyRoomService.java
@@ -1,14 +1,19 @@
 package com.deepnyangning.capstonebe.domain.studyroom.service;
 
 import com.deepnyangning.capstonebe.domain.studyroom.dto.StudyRoomResponse;
+import com.deepnyangning.capstonebe.domain.studyroom.entity.StudyRoom;
 import com.deepnyangning.capstonebe.domain.studyroom.mapper.StudyRoomMapper;
 import com.deepnyangning.capstonebe.domain.studyroom.repository.StudyRoomRepository;
 import com.deepnyangning.capstonebe.global.code.ErrorCode;
 import com.deepnyangning.capstonebe.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 
 @Service
 @RequiredArgsConstructor
@@ -16,8 +21,13 @@ public class StudyRoomService {
     private final StudyRoomRepository studyRoomRepository;
     private final StudyRoomMapper studyRoomMapper;
 
-    public StudyRoomResponse findStudyRoom(Long id){
-        return studyRoomMapper.toResponseDto(studyRoomRepository.findById(id)
-                .orElseThrow(() -> new CustomException(ErrorCode.STUDY_ROOM_NOT_FOUND)));
+    public StudyRoom findStudyRoomById(Long id){
+        return studyRoomRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.STUDY_ROOM_NOT_FOUND));
+    }
+
+    public Page<StudyRoomResponse> findAvailableStudyRooms(LocalDate date, LocalTime startTime, LocalTime endTime, Pageable pageable){
+        return studyRoomRepository.findAvailableStudyRooms(date, startTime, endTime, pageable)
+                .map(studyRoomMapper::toResponseDto);
     }
 }
