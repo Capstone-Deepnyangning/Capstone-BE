6fd0e506925e4d50763bf02b56f9af37f640e801
장윤정 <wkddbs3935@naver.com>
2025-04-13 03:58:32 +0900

    fix: 예약 중복 검사 쿼리 Lock 적용

--- src/main/java/com/deepnyangning/capstonebe/domain/studyroom/repository/StudyRoomReservationRepository.java
+++ src/main/java/com/deepnyangning/capstonebe/domain/studyroom/repository/StudyRoomReservationRepository.java
@@ -4,9 +4,11 @@ import com.deepnyangning.capstonebe.domain.studyroom.entity.ReservationStatus;
 import com.deepnyangning.capstonebe.domain.studyroom.entity.StudyRoom;
 import com.deepnyangning.capstonebe.domain.studyroom.entity.StudyRoomReservation;
 import com.deepnyangning.capstonebe.domain.user.entity.User;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -22,6 +24,7 @@ public interface StudyRoomReservationRepository extends JpaRepository<StudyRoomR
     Page<StudyRoomReservation> findStudyRoomReservationsByUser(User user, Pageable pageable);
     List<StudyRoomReservation> findByDateAndEndTimeLessThanEqualAndStatus(LocalDate date, LocalTime time, ReservationStatus status);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT r FROM StudyRoomReservation r " +
             "WHERE r.studyRoom.id = :studyRoomId AND r.date = :date " +
             "AND r.status = :status " +
