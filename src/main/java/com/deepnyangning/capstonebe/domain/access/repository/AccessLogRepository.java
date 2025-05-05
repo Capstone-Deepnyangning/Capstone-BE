98b976b8fc8cef93c5af49ba274f511cf19b7f22
장윤정 <wkddbs3935@naver.com>
2025-05-05 19:04:29 +0900

    feat: 7일 이전 로그 자동 삭제 스케줄러 구현

--- src/main/java/com/deepnyangning/capstonebe/domain/access/repository/AccessLogRepository.java
+++ src/main/java/com/deepnyangning/capstonebe/domain/access/repository/AccessLogRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface AccessLogRepository extends JpaRepository<AccessLog, Long>, JpaSpecificationExecutor<AccessLog> {
+    int deleteByAccessTimeBefore(LocalDateTime before);
 }
--- src/main/java/com/deepnyangning/capstonebe/domain/access/repository/FailLogRepository.java
+++ src/main/java/com/deepnyangning/capstonebe/domain/access/repository/FailLogRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface FailLogRepository extends JpaRepository<FailLog, Long>, JpaSpecificationExecutor<FailLog> {
+    int deleteByAccessTimeBefore(LocalDateTime before);
 }
