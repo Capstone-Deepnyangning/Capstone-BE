5464c1a7557d1e2ca6d30fb3a8aadc178883f4c8
장윤정 <wkddbs3935@naver.com>
2025-05-16 22:09:04 +0900

    feat: 이벤트 리스너 보완

--- src/main/java/com/deepnyangning/capstonebe/domain/face/event/FaceIssueReportEventListener.java
+++ src/main/java/com/deepnyangning/capstonebe/domain/face/event/FaceIssueReportEventListener.java
@@ -5,6 +5,8 @@ import com.deepnyangning.capstonebe.domain.notification.service.NotificationServ
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Component;
 public class FaceIssueReportEventListener {
     private final NotificationService notificationService;
 
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void onFaceIssueReportCreated(FaceIssueReportCreatedEvent event){
         try{
             notificationService.sendToAdmin(PushType.FACE_ISSUE_REPORTED);
