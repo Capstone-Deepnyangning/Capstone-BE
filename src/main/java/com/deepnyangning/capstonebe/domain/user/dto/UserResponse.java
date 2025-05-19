218b560b294c877b315c5b06b96b4d5e948f18d6
장윤정 <wkddbs3935@naver.com>
2025-05-19 21:42:05 +0900

    feat: user에 isFaceRegistered 정보 추가

--- src/main/java/com/deepnyangning/capstonebe/domain/user/dto/UserResponse.java
+++ src/main/java/com/deepnyangning/capstonebe/domain/user/dto/UserResponse.java
@@ -12,4 +12,6 @@ public class UserResponse {
     private String identifier;
 
     private String name;
+
+    private boolean faceRegistered;
 }
--- src/main/java/com/deepnyangning/capstonebe/domain/user/entity/User.java
+++ src/main/java/com/deepnyangning/capstonebe/domain/user/entity/User.java
@@ -27,4 +27,8 @@ public class User extends BaseEntity {
 
     @Builder.Default
     private boolean isActive = true;
+
+    @Builder.Default
+    private boolean isFaceRegistered = false;
+
 }
