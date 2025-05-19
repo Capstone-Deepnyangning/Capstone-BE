a4421d4827a0ce043dd7b605eaab09c0dacb11e3
장윤정 <wkddbs3935@naver.com>
2025-05-20 04:02:54 +0900

    refactor: 스터디룸 상세 조회 dto 수정

--- src/main/java/com/deepnyangning/capstonebe/domain/studyroom/controller/StudyRoomController.java
+++ src/main/java/com/deepnyangning/capstonebe/domain/studyroom/controller/StudyRoomController.java
@@ -1,6 +1,7 @@
 package com.deepnyangning.capstonebe.domain.studyroom.controller;
 
 import com.deepnyangning.capstonebe.domain.studyroom.dto.StudyRoomResponse;
+import com.deepnyangning.capstonebe.domain.studyroom.dto.StudyRoomSimpleResponse;
 import com.deepnyangning.capstonebe.domain.studyroom.mapper.StudyRoomMapper;
 import com.deepnyangning.capstonebe.domain.studyroom.service.StudyRoomService;
 import com.deepnyangning.capstonebe.global.response.ApiResponse;
@@ -28,10 +29,10 @@ public class StudyRoomController {
     private final StudyRoomMapper studyRoomMapper;
 
     @GetMapping("/studyrooms/{id}")
-    public ResponseEntity<ApiResponse<StudyRoomResponse>> getStudyRoom(@PathVariable Long id){
-        StudyRoomResponse studyRoomResponse = studyRoomMapper.toResponseDto(studyRoomService.findStudyRoomById(id));
+    public ResponseEntity<ApiResponse<StudyRoomSimpleResponse>> getStudyRoom(@PathVariable Long id){
+        StudyRoomSimpleResponse studyRoomResponse = studyRoomMapper.toSimpleDto(studyRoomService.findStudyRoomById(id));
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.<StudyRoomResponse>builder().result(studyRoomResponse).success(true).code(200).message("스터디룸 상세 조회에 성공했습니다.").build());
+                .body(ApiResponse.<StudyRoomSimpleResponse>builder().result(studyRoomResponse).success(true).code(200).message("스터디룸 상세 조회에 성공했습니다.").build());
     }
 
     @GetMapping("/studyrooms")
--- src/main/java/com/deepnyangning/capstonebe/domain/studyroom/mapper/StudyRoomMapper.java
+++ src/main/java/com/deepnyangning/capstonebe/domain/studyroom/mapper/StudyRoomMapper.java
@@ -1,10 +1,12 @@
 package com.deepnyangning.capstonebe.domain.studyroom.mapper;
 
 import com.deepnyangning.capstonebe.domain.studyroom.dto.StudyRoomResponse;
+import com.deepnyangning.capstonebe.domain.studyroom.dto.StudyRoomSimpleResponse;
 import com.deepnyangning.capstonebe.domain.studyroom.entity.StudyRoom;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface StudyRoomMapper {
     StudyRoomResponse toResponseDto(StudyRoom studyRoom);
+    StudyRoomSimpleResponse toSimpleDto(StudyRoom studyRoom);
 }
