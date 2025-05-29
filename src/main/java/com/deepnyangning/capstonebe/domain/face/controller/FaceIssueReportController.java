9540a49420631b22c9f03a5fb1622a3e075e291a
장윤정 <wkddbs3935@naver.com>
2025-05-29 15:15:41 +0900

    feat: 안면인식 신고 조회 반환값 수정

--- src/main/java/com/deepnyangning/capstonebe/domain/face/controller/FaceIssueReportController.java
+++ src/main/java/com/deepnyangning/capstonebe/domain/face/controller/FaceIssueReportController.java
@@ -1,5 +1,6 @@
 package com.deepnyangning.capstonebe.domain.face.controller;
 
+import com.deepnyangning.capstonebe.domain.face.dto.FaceIssuePreviewResponse;
 import com.deepnyangning.capstonebe.domain.face.dto.FaceIssueReportResponse;
 import com.deepnyangning.capstonebe.domain.face.service.FaceIssueReportService;
 import com.deepnyangning.capstonebe.global.response.ApiResponse;
@@ -30,11 +31,11 @@ public class FaceIssueReportController implements FaceIssueReportControllerDocs 
     }
 
     @GetMapping("/admin/reports")
-    public ResponseEntity<ApiResponse<Page<FaceIssueReportResponse>>> getFaceIssueReports(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "7") int size){
+    public ResponseEntity<ApiResponse<Page<FaceIssuePreviewResponse>>> getFaceIssueReports(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "7") int size){
         Pageable pageable = PageRequest.of(page, size);
-        Page<FaceIssueReportResponse> response = faceIssueReportService.findFaceIssueReports(pageable);
+        Page<FaceIssuePreviewResponse> response = faceIssueReportService.findFaceIssueReports(pageable);
         return ResponseEntity.status(HttpStatus.OK)
-                .body(ApiResponse.<Page<FaceIssueReportResponse>>builder().result(response).success(true).code(200).message("안면 인식 문제 신고 목록을 성공적으로 조회했습니다.").build());
+                .body(ApiResponse.<Page<FaceIssuePreviewResponse>>builder().result(response).success(true).code(200).message("안면 인식 문제 신고 목록을 성공적으로 조회했습니다.").build());
     }
 
     @GetMapping("/admin/reports/{reportId}")
